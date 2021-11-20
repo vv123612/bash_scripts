35b6592396e3759bcad3803d0c474cfff98bd5bc
Vitaly <v123612@gmail.com>
2021-11-20 11:40:59 +0300

    python_install - delete sudo

--- python_install.sh
+++ python_install.sh
@@ -8,8 +8,8 @@ python_version_short="3.9"
 # Step 1 – Installing Prerequisite
 # As you are going to install Python 3.8 from the source. You need to install some development libraries to compile Python source code. Use the following command to install prerequisites for Python:
 
-sudo apt-get install build-essential checkinstall
-sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
+apt-get install build-essential checkinstall
+apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
     libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
 
 # Step 2 – Download Python 3.8
@@ -18,15 +18,15 @@ sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
 cd /opt
 sudo wget https://www.python.org/ftp/python/${python_version}/Python-${python_version}.tgz
 # Then extract the downloaded source archive file
-sudo tar xzf Python-${python_version}.tgz
+tar xzf Python-${python_version}.tgz
 
 # Step 3 – Compile Python Source
 # Use the below set of commands to compile Python source code on your system using the altinstall command.
 
 cd Python-${python_version}
-sudo ./configure --enable-optimizations
-sudo make altinstall
-make altinstall 
+./configure --enable-optimizations
+make altinstall
+
 # is used to prevent replacing the default python binary file /usr/bin/python.
 
 # Step 4 – Check Python Version
@@ -38,4 +38,4 @@ python{python_version_short} -V
 # After successful installation remove the downloaded archive to save disk space
 
 cd /opt
-sudo rm -f Python-${python_version}.tgz
+rm -f Python-${python_version}.tgz
