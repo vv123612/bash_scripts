1356726f82f2300474b6c27ea7bd9677b135a264
vitaly <v123612@gmail.com>
2022-02-18 12:10:12 +0300

    git example

--- git_example.sh
+++ git_example.sh
@@ -1,12 +1,44 @@
 #!/bin/bash
 
+
+function create_branch() {
+  
+  local commits_count=$1
+  local branch_name=$2
+
+  git checkout $branch_name
+  git branch 
+
+  for (( i=1; i<=$commits_count; i++ ))
+  do  
+    message="${branch_name}_${i}"
+    echo $message
+    echo $message >> f.txt
+    git add .
+    git commit -m "$message"
+
+done  
+}
+
+
+GIT_DIR=".git"
+if [ -d "$GIT_DIR" ]; 
+    then rm -Rf $GIT_DIR; 
+fi
+
+
 git init
-for (( i=1; i<=9; i++ ))
-do  
-  echo "$i" >> f.txt
-  git add .
-  git commit -m "$i"
+echo "0" > f.txt
+git add .
+git commit -m "Init"
 
-  #git branch "b$i"
+#create branches
+# git branch 
+git checkout -b n
+git checkout -b k
+git checkout -b master
 
-done
+#create commits
+create_branch 3 n
+create_branch 3 k
+create_branch 3 master
