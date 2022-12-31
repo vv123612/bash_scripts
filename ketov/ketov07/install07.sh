9d55956ca336a372dc02721649c3597faf383c5b
vitaly <v123612@gmail.com>
2022-12-31 23:21:44 +0300

    1

---
+++ ketov/ketov07/install07.sh
@@ -0,0 +1,81 @@
+sudo apt update                                                                                                                          
+
+sudo apt install perl gcc make
+sudo ./VBoxLinuxAdditions.run 
+sudo adduser $USER vboxsf
+sudo snap install code --classic
+sudo apt install -y build-essential gdb
+sudo apt-get install -y build-essential 
+
+
+git clone https://github.com/vv123612/c.git
+
+sudo apt-get install -y qtcreator
+sudo apt-get install -y qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
+
+
+
+sudo apt install xnest
+Xnest 
+Xnest :0 &
+
+
+
+Xnet :0 --listen tcp &
+Xnest :1 --listen tcp &
+Xnest :1 -listen tcp &
+# lsof -p 115888 -a -i
+# DISPLAY=:1 strace -fe conneect xeyes
+DISPLAY=:1 strace -fe connect xeyes
+xeyes
+
+# export DISPLAY=:1
+xeyes
+
+xterm & xcalc & xeyes &
+jobs
+fg %3
+sudo apt install xterm
+xterm & 
+
+sudo apt install twm
+twm &
+jobs
+Xnest :1 -listen tcp
+
+htop
+olwm
+
+mwm &
+sudo apt install mwm
+mwm &
+# kill -9 &mwm
+
+mwm &
+
+sudo apt install icewm
+icewm-session &
+icewm-session --replace &
+
+
+# sudo add-apt-repository ppa:hizo/mkv-extractor-gui
+# sudo apt install mkv-extractor-qt5
+# dpkg -L mkv-extractor-qt5
+# dpkg -L mkv-extractor-qt5
+
+sudo apt install gnome-session-bin
+echo DISPLAY
+echo $DISPLAY
+gnome-session &
+Xnest :7 -listen tcp &
+export DISPLAY=:7
+gnome-session &
+Xerhyr
+sudo apt install xserver-xephyr
+DISPLAY=:0 Xephyr :1 -listen tcp &
+DISPLAY=:1 Xephyr :1 -listen tcp &
+DISPLAY=:0 Xephyr :1 -listen tcp &
+DISPLAY=:0 Xephyr :3 -listen tcp &
+startkde
+
+
