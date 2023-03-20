ceb66308d931ff9c36d96866d460592e50cf2b6f
vitaly <v123612@gmail.com>
2023-03-20 14:56:23 +0300

    1

--- gentoo/1ssh_chroot.sh
+++ gentoo/1ssh_chroot.sh
@@ -2,6 +2,14 @@ rc-service sshd start
 
 echo 'root:root' | chpasswd
 
+mount /dev/sda3 /mnt/gentoo
+
+mirrorselect -i -o >> /mnt/gentoo/etc/portage/make.conf
+mkdir --parents /mnt/gentoo/etc/portage/repos.conf
+cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
+cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+
+
 mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
